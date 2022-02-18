761ea27711fe90d23a285d8125bb1481fb24afb6
Adanurk <gumussoykbr@gmail.com>
2022-02-18 23:20:38 +0100

    some explanations added

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ const days = document.getElementById("days");
 const hours = document.querySelector("#hours");
 const minutes = document.querySelector("#minutes");
 const seconds = document.querySelector("#seconds");
+
+const loading = document.querySelector(".loading");
 const countdown = document.querySelector("#countdown");
 
+//This  is the first loading effect:
+
 window.addEventListener("load", () => {
-    
-})
+    loading.style.display = "block";
+    //miliseconds in set Timeout
+    setTimeout(()=>{
+        loading.style.display = "none";
+    }, 2000)
+});
 
