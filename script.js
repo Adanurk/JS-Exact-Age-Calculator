f7f14857285b7230cc01d1a61fc7922c3f2304bd
Adanurk <gumussoykbr@gmail.com>
2022-02-18 23:13:13 +0100

    small changes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// loading => onload - 1second loading effect
+// after calculation - 1 second loading effect
+
+
+
 const years = document.getElementById("years");
 const months = document.getElementById("months");
 const days = document.getElementById("days");
@@ -5,4 +10,8 @@ const hours = document.querySelector("#hours");
 const minutes = document.querySelector("#minutes");
 const seconds = document.querySelector("#seconds");
 const countdown = document.querySelector("#countdown");
+
+window.addEventListener("load", () => {
+    
+})
 
