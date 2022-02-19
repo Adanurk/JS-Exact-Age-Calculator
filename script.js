10be718ac060489dfb941fa055d302ed2b56b22d
Adanurk <gumussoykbr@gmail.com>
2022-02-19 01:04:55 +0100

    mostly done

--- script.js
+++ script.js
@@ -61,16 +61,58 @@ let selectedBirthday = new Date();
 let birthdayInput = document.querySelector("input[name=birthday]");
 birthdayInput.addEventListener("change", (event) => {
     console.log(event.target.value);
+// event.target.value == birthdayInput.value
 // so I reached the event object
 //convert to date from dateString
     selectedBirthday = new Date(event.target.value);
     if(selectedBirthday > new Date()){
-        alert("Your birthday cannot be later than today!")
+        alert("Your birthday cannot be later than today!");
+        return;
     }
+    document.body.style.backgroundImage = "url('https:/images.unsplash.com/photo-1467810563316-b5476525c0f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80')";
 
+    loading.style.display = "block";
+    setInterval(updateCountdown, 1000);
+    // I added an interval to my function with setInternal
+    setTimeout(()=>{
+        loading.style.display = "none";
+    }, 1000);
 
-})
+});
 
+const updateCountdown = () => {
+    let dobYear = selectedBirthday.getFullYear();
+    let dobMonth = selectedBirthday.getMonth();
+    let dobDay = selectedBirthday.getDate();
+    let dobHours = selectedBirthday.getHours();
+    let dobMinutes = selectedBirthday.getMinutes();
+    let dobSeconds = selectedBirthday.getSeconds();
+
+    let now = new Date();
+
+    let currentYear = now.getFullYear();
+    let currentMonth = now.getMonth();
+    let currentDay = now.getDate();
+    let currentHour = now.getHours();
+    let currentMinute = now.getMinutes();
+    let currentSecond = now.getSeconds();
+
+    let yearOfAge = currentYear - dobYear;
+    let monthOfAge = currentMonth - dobMonth;
+    let dayOfAge = currentDay - dobDay;
+    let hourOfAge = currentHour - dobHours;
+    let minuteOfAge = currentMinute - dobMinutes;
+    let secondOfAge = currentSecond - dobSeconds;
+
+    //Adding values to DOM
+
+    years.innerHTML = yearOfAge.toString().padStart(2, "0");
+    months.innerHTML = monthOfAge.toString().padStart(2, "0");
+    days.innerHTML = dayOfAge.toString().padStart(2, "0");
+    hours.innerHTML = hourOfAge.toString().padStart(2, "0");
+    minutes.innerHTML = minuteOfAge.toString().padStart(2, "0");
+    seconds.innerHTML = secondOfAge.toString().padStart(2, "0");
+}
 
 
 
